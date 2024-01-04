dcb6d2b87717b4bcfdbd66a5f1ac1e5e7b4c9e4b
YouennPJ <youenn.pn@gmail.com>
2024-01-04 13:56:36 -0500

    First commit

---
+++ .zsh/cli.zsh
@@ -0,0 +1,8 @@
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/ (\1)/p'
+}
+setopt PROMPT_SUBST
+export PROMPT='%F{green}%1d%f%F{red}$(parse_git_branch)%f:~$ '
+export PATH="$PATH:/opt/homebrew/bin"
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
