ce49eade3c087eb273b4cbf5f8b879aebf096b9e
YouennPJ <youenn.pn@gmail.com>
2024-01-04 14:51:01 -0500

    Add alias to edit dotfiles

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias la="ls -a"
-alias vimconf="cd ~/dotfiles/.config/nvim/; nvim .; cd ~; ./dotfiles/make.sh"
+alias vimconf="cd ~/dotfiles/.config/nvim/; nvim .; cd ~; ./dotfiles/make.sh; source ~/.zshrc; clear"
+alias dotfiles="cd ~/dotfiles/; nvim .; cd ~; ./dotfiles/make.sh; source ~/.zshrc; clear"
 alias code="cd ~/Personnal; clear; ls"
